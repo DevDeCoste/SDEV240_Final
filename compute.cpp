db32b65c7d128002074cb764f30b29723516d8f4
Dev <DevDeCoste@users.noreply.github.com>
2020-05-01 11:45:05 -0400

    Properly configured compute class and header file and got to work

--- compute.cpp
+++ compute.cpp
@@ -1,3 +1,33 @@
 #include "compute.h"
+#include <iostream>
+#include <stdlib.h>
+#include <stdexcept> 
+
+using namespace std;
+
+double compute::add(double a, double b) {
+    double total = a + b;
+    std::cout << "Sum: " << total << std::endl;
+    return a + b;
+}
+
+double compute::subtract(double a, double b) {
+    double total = a - b;
+    std::cout << "Difference: " << total << std::endl;
+    return a - b;
+}
+
+double compute::multiply(double a, double b) {
+    double total = a * b;
+    std::cout << "Product: " << total << std::endl;
+    return a * b;
+}
+
+double compute::divide(double a, double b) {
+
+    double total = a / b;
+    std::cout << "Quotient: " << total << std::endl;
+    return a / b;
+}
 
 
--- compute.h
+++ compute.h
@@ -1,4 +1,4 @@
-#pragma once
+#define COMPUTE_H
 
 #include <iostream>
 #include <sstream>
@@ -6,33 +6,16 @@
 #include <stdexcept>
 
 
+
+
 class compute
 {
 public:
 
-    double add(double a, double b) {
-        double total = a + b;
-        std::cout << "Sum: " << total << std::endl;
-        return a + b;
-    }
+    double add(double a, double b);
+    double subtract(double a, double b);
+    double multiply(double a, double b);
+    double divide(double a, double b);
 
-    double subtract(double a, double b) {
-        double total = a - b;
-        std::cout << "Difference: " << total << std::endl;
-        return a - b;
-    }
-
-    double multiply(double a, double b) {
-        double total = a * b;
-        std::cout << "Product: " << total << std::endl;
-        return a * b;
-    }
-
-    double divide(double a, double b) {
-
-        double total = a / b;
-        std::cout << "Quotient: " << total << std::endl;
-        return a / b;
-    }
 };
 
